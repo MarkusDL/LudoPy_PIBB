06c853714f5f7111280bf8c8b146bfcc03a2de3f
MarkusDL <37587328+MarkusDL@users.noreply.github.com>
2021-04-12 13:12:03 +0200

    Update Agent.py

--- LudoPIBB/Agent.py
+++ LudoPIBB/Agent.py
@@ -29,15 +29,25 @@ class Agent:
         self.get_weights()
 
     def get_weights(self):
+        #messy but works, possibly a way better implementation of all weight 
+        # get/set functions as a lot of possible redundant lists are used
         i = 0
+        # list for shape of params pr. layer bias and weigth , all the parameters and the number of params pr layer weight and bias seperate
         shapes = []
         params = []
         len_params = []
+        
         for  name, param in self.nn.named_parameters():
+            # read weight or bias from layer
             param = param.detach().numpy()
+            
+            #save shape 
             shapes.append(param.shape)
+            # save number of parameters
             len_params.append(np.product(param.shape))
+            # add to list of all params
             params.append(np.ravel(param))
+        # combine to single numpy array and save    
         self.params = np.concatenate(params)
         self.shape_params = shapes
         self.len_params = len_params
@@ -45,6 +55,7 @@ class Agent:
         return self.params
 
     def get_action(self, state):
+        #pass state through network
         input = torch.from_numpy(state).float()
         return torch.argmax(self.nn(input))
 
@@ -56,6 +67,8 @@ class Agent:
         with torch.no_grad():
             i = 1
             for name, param in self.nn.named_parameters():
+                # set param to the part of the list of total weights
                 param_weights = weights[sum(self.len_params[:i-1]):sum(self.len_params[:i])]
+                # reshape it to original shape after ravel and copy to the parameter
                 param.copy_(torch.from_numpy(np.resize(param_weights, self.shape_params[i-1])))
                 i += 1
